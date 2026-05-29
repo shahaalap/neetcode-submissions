9ecee771e6dc3570879378d56f0d953431d1eee0
shahaalap <68823276+shahaalap@users.noreply.github.com>
2026-05-29 11:31:47 -0400

    Update: spiral-matrix - submission-0

--- Data Structures & Algorithms/spiral-matrix/submission-0.py
+++ Data Structures & Algorithms/spiral-matrix/submission-0.py
@@ -1,32 +1,25 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        i, j , k , l = 0, len(matrix) - 1, 0, len(matrix[0]) -1
+        i, j , k , l = 0, len(matrix[0]) - 1 , 0, len(matrix) - 1
         result = []
+        while i <= j and k <= l:
+            for col in range(i, j + 1):
+                result.append(matrix[k][col])
+            k += 1
+            
+            if k > l: break
+            for row in range(k, l + 1):
+                result.append(matrix[row][j])
+            j -= 1
 
-        while i <= j  and k <= l :
-            for col in range(k, l + 1):
-                result.append(matrix[i][col])
-            
-            i += 1
-            
-            for row in range(i,j + 1):
-                result.append(matrix[row][l])
-            
+            if i > j: break
+            for col in range(j, i - 1, -1):
+                result.append(matrix[l][col])
             l -= 1
 
-            if i > j  or k > l:
-                break
-                
-            for col in range(l , k - 1, -1):
-                result.append(matrix[j][col])
-            
-            j -= 1
-            
-            for row in range(j , i - 1, - 1):
-                result.append(matrix[row][k])
-            
-            k += 1
+            if k > l: break
+            for row in range(l, k - 1, -1):
+                result.append(matrix[row][i])
+            i += 1
 
         return result
-
-
